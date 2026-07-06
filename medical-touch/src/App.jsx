4905794adac3f258a4348bff93b62db73f530a7f
youseftarda <youseftarda77@gmail.com>
2026-07-06 18:53:05 +0300

    Remove InstallPrompt auto-popup, keep only Navbar install button

--- medical-touch/src/App.jsx
+++ medical-touch/src/App.jsx
@@ -4,7 +4,6 @@ import { CartProvider } from './context/CartContext.jsx'
 import { AuthProvider, useAuth } from './context/AuthContext.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import InstallPrompt from './components/InstallPrompt.jsx'
 import HomePage from './pages/HomePage.jsx'
 import CategoryPage from './pages/CategoryPage.jsx'
 import ProductDetailPage from './pages/ProductDetailPage.jsx'
@@ -79,7 +78,6 @@ function AppContent() {
                 </Routes>
               </main>
               <Footer />
-              <InstallPrompt />
             </>
           }
         />
